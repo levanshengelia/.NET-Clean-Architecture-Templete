cab880c23892d400cc9292ee12d35703891ab62e
levani836 <levani.shengelia@makingscience.com>
2025-09-01 15:59:05 +0400

    Add projects and tests with WireMock.NET

---
+++ src/Api/Program.cs
@@ -0,0 +1,32 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+var url = builder.Configuration.GetSection("ExternalServiceSettings:Service1:BaseUrl").Get<string>();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
+public partial class Program { } // For tests
---
+++ tests/Api.IntegrationTests/TestExamples/Test.cs
@@ -0,0 +1,66 @@
+﻿using WireMock.Server;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using Xunit.Abstractions;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+
+namespace Api.IntegrationTests.TestExamples;
+
+public class Test(ITestOutputHelper output)
+{
+    [Fact]
+    internal async Task This_is_test_example()
+    {
+        var client = GetClientWithMockedExternalServices();
+
+        var response = await client.GetAsync("/hello");
+        var content = await response.Content.ReadAsStringAsync();
+        output.WriteLine(content);
+        Assert.True(response.IsSuccessStatusCode);
+    }
+
+    private static HttpClient GetClientWithMockedExternalServices()
+    {
+        var externalService = WireMockServer.Start();
+
+        var fakeBody = new
+        {
+            Message = "Hello"
+        };
+
+        var fakeResponse = new
+        {
+            Message = "Hello",
+        };
+
+        // POST
+        externalService
+            .Given(Request.Create()
+                .WithPath("/path1")
+                .UsingPost()
+                .WithBody(JsonConvert.SerializeObject(fakeBody)))
+            .RespondWith(Response.Create()
+                .WithStatusCode(200)
+                .WithBody(JsonConvert.SerializeObject(fakeResponse)));
+        
+        // GET
+        externalService
+            .Given(Request.Create().WithPath("/hello").UsingGet())
+            .RespondWith(Response.Create()
+                .WithStatusCode(200)
+                .WithBody(JsonConvert.SerializeObject(fakeResponse)));
+
+        return new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureAppConfiguration((context, configBuilder) =>
+            {
+                configBuilder.AddInMemoryCollection(new Dictionary<string, string?>
+                {
+                    ["ExternalServiceSettings:Service1:BaseUrl"] = externalService.Url
+                });
+            });
+        }).CreateClient();
+    }
+}
